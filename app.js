e48342f805400648f1e20be495d2dc7b2b418d1e
Ervin Young <ervinyoung24@gmail.com>
2018-07-27 12:04:46 -0700

    created jquery hide/show carousel + improved about pg

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 console.log("hello world");
 
 $(document).ready(function(){
-  $('.noboy').css('color','red');
-  $
+  console.log('jquery sanity check');
+    $('#c2').hide();
+    $('#c3').hide();
 
+    $('#firstCar').click(function(){
+      console.log('1st');
+      $('#c1').show();
+      $('#c2').hide();
+      $('#c3').hide();
+    });
+    $('#secondCar').click(function(){
+      console.log('2nd');
+      $('#c1').hide();
+      $('#c2').show();
+      $('#c3').hide();
+    });
+    $('#thirdCar').click(function(){
+      console.log('3rd');
+      $('#c1').hide();
+      $('#c2').hide();
+      $('#c3').show();
+    });
 
 }) //close document.ready
